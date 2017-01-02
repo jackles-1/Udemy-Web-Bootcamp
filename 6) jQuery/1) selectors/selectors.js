1e65b610dfc67eef8ca8346a2048e407d4e559fe
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-02 13:01:30 -0700

    finish jQuery selectors demo, begin methods demo until attr

---
+++ 6) jQuery/1) selectors/selectors.js
@@ -0,0 +1,12 @@
+// select all div's and give them a purple background
+$("div").css("background", "purple");
+
+// select the divs with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+
+// select the div with id "third" and give it an orange border
+$("#third").css("border", "orange solid 2px");
+
+// select the first div and change its font color to pink
+$("div:first-of-type").css("color", "pink");
+
