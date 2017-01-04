2d2b5ce983e2dffa0d4ce371caf46007ca318321
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-04 16:29:53 -0700

    finish jQuery events demo

--- 6) jQuery/3) events/events.js
+++ 6) jQuery/3) events/events.js
@@ -1,4 +1,3 @@
-
 // demonstrates keypress function and "event" object
 $("input").keypress(function(event){
 	console.log(event);
@@ -6,4 +5,18 @@ $("input").keypress(function(event){
 	{
 		alert("You hit ENTER");
 	}
+});
+
+// on() demonstrations
+$("h1").on("click", function(){
+	$(this).css("color", "purple");
+});
+
+// mouseenter and mouseleave can be replaced by CSS's hover
+$("button").on("mouseenter", function(){
+	$this.css("font-weight", "bold")
+});
+
+$("button").on("mouseleave", function(){
+	$this.css("font-weight", "normal")
 });
